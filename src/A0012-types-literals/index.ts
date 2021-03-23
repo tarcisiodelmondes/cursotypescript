bd2652c33b105aaf96de28dc16e4722f168f3939
Tarcisio Delmondes <tarcisiodelmondes@gmail.com>
2021-03-23 09:55:16 -0300

    types-literals

---
+++ src/A0012-types-literals/index.ts
@@ -0,0 +1,17 @@
+let x = 10; // eslint-disable-line
+x = 0b1010;
+const y = 100;
+const a = 100;
+
+const person = {
+  name: 'Tarcisio' as const,
+  lastname: 'Delmondes',
+};
+
+function choiceColor(cor: 'RED' | 'YELLOW' | 'BLUE') {
+  return cor;
+}
+
+console.log(choiceColor('BLUE'));
+
+export default 1;
