c0f9de897ad37cf445095b56de8bd51903a9e3d9
Tarcisio Delmondes <tarcisiodelmondes@gmail.com>
2021-03-24 19:18:00 -0300

    super

--- src/A0021-heritage/index.ts
+++ src/A0021-heritage/index.ts
@@ -7,20 +7,26 @@ export class Person {
 }
 
 export class Student extends Person {
+  constructor(name: string, lastName: string, public room: string) {
+    super(name, lastName);
+  }
   getFullName(): string {
-    return `This comes of the student ${this.name} ${this.lastName}`;
+    return `This comes of the student ${this.name} ${this.lastName} ${this.room}`;
   }
 }
 
 export class Client extends Person {
+  constructor(name: string, lastName: string, public room: string) {
+    super(name, lastName);
+  }
   getFullName(): string {
-    return `This comes of the client ${this.name} ${this.lastName}`;
+    return `This comes of the client ${this.name} ${this.lastName} ${this.room}`;
   }
 }
 
 const person = new Person('Tarcisio', 'Delmondes');
-const student = new Student('Julia', 'Alves');
-const client = new Client('Maria', 'Silva');
+const student = new Student('Julia', 'Alves', 'A1');
+const client = new Client('Maria', 'Silva', 'AB');
 
 console.log(person.getFullName());
 console.log(student.getFullName());
